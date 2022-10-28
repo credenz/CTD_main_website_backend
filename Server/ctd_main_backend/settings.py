fa5e9de57912d4abd2a26bc6cad9c7be26bea034
Harsh Bhat <harshbhat83@gmail.com>
2022-10-28 14:10:00 +0530

    auth passwords removed

--- Server/ctd_main_backend/settings.py
+++ Server/ctd_main_backend/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+# Comment these
 # AUTH_PASSWORD_VALIDATORS = [
 #     {
 #         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
