54effa94ae338545e6c537e857143bf5b30e8e70
unknown <chandolkarshreyas001@gmail.com>
2022-10-30 17:58:37 +0530

    suryaOP

--- Server/core/admin.py
+++ Server/core/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
             "fields": (
-                ('username'), ('first_name', 'last_name'), ('email'), 'is_active'
+                ('username'), ('first_name', 'last_name'), ('email'), ('is_superuser','is_staff')
             ),
         }),
     )
